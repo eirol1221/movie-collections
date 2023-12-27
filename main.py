31d2bd9c24966a13831b29da67c7c2c927aec374
Lorie Lie Cubid <eirol1221@gmail.com>
2023-12-27 09:46:48 +0800

    Revised to use an api for the movies; added select page

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+import os
+from datetime import datetime
+
+TMDB_KEY = os.environ['TMDB_KEY']
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'YOUR SECRET KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movie-collections.db'
 db.init_app(app)
 Bootstrap5(app)
@@ -39,6 +43,9 @@ class AddForm(FlaskForm):
     submit = SubmitField("Add Movie")
 
 with app.app_context():
+    # ---- CREATE NEW DB ---- #
+    # db.create_all()
+
     # ---- ADD FIRST RECORD ---- #
     # db.session.add(Movie(
     #     title="Phone Booth",
@@ -62,6 +69,24 @@ with app.app_context():
     #     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
     # ))
     # db.session.commit()
+
+    def search_movie(title):
+        search_url = f"https://api.themoviedb.org/3/search/movie?api_key=<YOUR API KEY>={title}"
+
+        response = requests.get(search_url)
+        # id = result['id']
+        # desc = result['overview']
+        # year = result['release_date'].split('-')[0]
+        # img_url = f"http://image.tmdb.org/t/p/original/{result['poster_path']}"
+        return response.json()['results']
+
+    def get_review(id):
+        review_url = f"https://api.themoviedb.org/3/movie/{id}/reviews?language=en-US&page=1&accept=application/json&api_key=<YOUR API KEY>"
+        response = requests.get(review_url)
+        result = response.json()['results']
+        review = result['content']
+        rating = result['author_details']['rating']
+        return (review, rating)
 
 
     @app.route("/")
@@ -95,25 +120,17 @@ with app.app_context():
 
     @app.route("/add", methods=['GET', 'POST'])
     def add_movie():
-        movie_url = "https://www.themoviedb.org/"
-
         form = AddForm()
 
         if form.validate_on_submit():
-            print(form.title.data)
+            movies = search_movie(form.title.data)
 
-
+            return render_template('select.html', movies=movies)
 
         return render_template("add.html", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-    # ---- CREATE NEW DB ---- #
-    # db.create_all()
 
 
 
